9859d9dfe175eeba76fd463d8145789d08f4930f
Seamus McGill <seamusmcgill@gmail.com>
2022-01-20 18:58:00 -0500

    Added printPassTimes function

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 // const { fetchMyIP, fetchCoordsByIP, fetchISSFlyoverTimes } = require('./iss');
 const { nextISSTimesForMyLocation } = require('./iss');
 
+const printPassTimes = (passTimes) => {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
   // success, print out the deets!
-  for (const time of passTimes) {
-    const datetime = new Date(0);
-    datetime.setUTCSeconds(time.risetime);
-    console.log(`Next pass at ${datetime} for ${time.duration} seconds!`);
-  }
+  printPassTimes(passTimes);
 });
 
